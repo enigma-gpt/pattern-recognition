ab63a2dd72493a7c19c51ff3fb72b1cb3e7820bb
Grzegorz Talaga <Grzegorz.Talaga@hltech.com>
2023-01-02 15:19:17 +0100

    init

---
+++ pattern-recognition-webapi/src/main/java/com/enigmagpt/learning/patternrecognitionwebapi/PatternRecognitionWebapiApplication.java
@@ -0,0 +1,13 @@
+package com.enigmagpt.learning.patternrecognitionwebapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatternRecognitionWebapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatternRecognitionWebapiApplication.class, args);
+	}
+
+}
