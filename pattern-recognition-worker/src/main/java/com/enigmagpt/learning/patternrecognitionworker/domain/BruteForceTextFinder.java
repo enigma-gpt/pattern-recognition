67e483f94ab00656bd537e66d6e0c88578df35d0
Grzegorz <grzechtl@gmail.com>
2023-01-04 19:59:30 +0100

    add test class

--- pattern-recognition-worker/src/main/java/com/enigmagpt/learning/patternrecognitionworker/domain/BruteForceTextFinder.java
+++ pattern-recognition-worker/src/main/java/com/enigmagpt/learning/patternrecognitionworker/domain/BruteForceTextFinder.java
@@ -4,6 +4,18 @@ public class BruteForceTextFinder implements TextFinder {
 
     @Override
     public Result find(String input, String pattern) {
+
+        char[] inputArr = input.toCharArray(); //rtdfgha
+        char[] patternArr = pattern.toCharArray(); //as
+
+        for (int i = 0; i < input.length() - 1; i++) {
+
+
+
+
+
+        }
+
         return null;
     }
 }
