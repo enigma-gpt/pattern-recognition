57d15277c87d16ad49560ae10a8e971d4b0fa82f
Grzegorz <grzechtl@gmail.com>
2023-01-04 18:11:09 +0100

    add Groovy and fix tests

---
+++ pattern-recognition-worker/src/test/groovy/com/enigmagpt/learning/patternrecognitionworker/ApplicationTest.groovy
@@ -0,0 +1,12 @@
+package com.enigmagpt.learning.patternrecognitionworker
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class ApplicationTest extends Specification {
+
+    def 'asdf'() {
+
+    }
+}
--- pattern-recognition-worker/src/test/java/com/enigmagpt/learning/patternrecognitionworker/ApplicationTests.java
+++ pattern-recognition-worker/src/test/java/com/enigmagpt/learning/patternrecognitionworker/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.enigmagpt.learning.patternrecognitionwebapi;
+package com.enigmagpt.learning.patternrecognitionworker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
